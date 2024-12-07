b8962429e9a56650442c9526be6ed1a3b47af560
Arkady <sheglovaoe@gmail.com>
2024-12-08 00:01:28 +0300

    export method

--- source/NetCoreServer/api/IHttpSession.cs
+++ source/NetCoreServer/api/IHttpSession.cs
@@ -16,4 +16,12 @@ public interface IHttpSession : ISession
     /// Get the HTTP response
     /// </summary>
     public HttpResponse Response { get; }
+
+    /// <summary>
+    /// Send the HTTP response (asynchronous)
+    /// </summary>
+    /// <param name="response">HTTP response</param>
+    /// <returns>'true' if the current HTTP response was successfully sent, 'false' if the session is not connected</returns>
+
+    bool SendResponseAsync(HttpResponse response);
 }
